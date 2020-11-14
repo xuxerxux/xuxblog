8867fd4308e90d14a0d3c8411453783a575604e9
xuxerxux <xuxerxux@gmail.com>
2020-11-14 19:44:48 +0300

    used http Get method

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 import(
 	"net/http"
+	"fmt"
 	)
 
 func main(){
 	resp, _ := http.Get("https://example.com")
-	// got the response!
+	for name, cont := range resp.Header {
+		fmt.Println(name, cont[0])
+		// how to work with slicses in http package
+	}
 }
