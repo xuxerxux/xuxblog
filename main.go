b25c8b24db5e4f680cc4e57525532d4932f36970
xuxerxux <xuxerxux@gmail.com>
2020-11-14 18:54:44 +0300

    attempt dial tcp and get content

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
-
+import(
+	"net"
+	"fmt"
+	"io"
+	"bytes"
+	)
 
 func main(){
-	var name string
-	name = " John"
-	println("Hello", name)
+	conn, _:= net.Dial("tcp","golang.org:80")
+	defer conn.Close()
+
+
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	var buf bytes.Buffer
+	io.Copy(&buf, conn)
+	fmt.Println(" belge: ", buf.String())
 	return
 }
