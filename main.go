8b3a8ab860f605b1d06db386ecc4f8cf9ed5a52a
xuxerxux <xuxerxux@gmail.com>
2020-11-27 22:53:46 +0300

    generated cert and used for TLS

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
-package main 
+package main
 
-func Hello(name string) string {
-	if name == "" {
-		name = "world"
+import(
+	"net/http"
+	"log"
+	"fmt"
+	"io/ioutil"
+	)
+
+type Post struct {
+	Title string
+	Body []byte 
+}
+
+
+func (p *Post) save() error{
+	filename := p.Title +".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+func loadPage(title string) *Post{
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return &Post{Title:"Hata", Body:[]byte("Böyle bir sayfa yok!")}
 	}
-	return "hello " + name
+	return &Post{Title: title, Body: body}
 }
 
-func main() {
-	println(Hello("WWWWWWWWWWWWWW"))
-	return
+func main(){
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServeTLS(":8000","cert.pem","key.pem",nil))
 }
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	baslik := r.URL.Path[len("/view/"):]
+	govde := loadPage(baslik)
+	fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>",baslik, govde.Body)
+	}
